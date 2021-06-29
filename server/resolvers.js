928f63d7a4b3a49edc994ab79677a6a41da4e158
Ali Bulut <ali@alibulut.net>
2021-06-29 15:54:21 +0300

    server: fetching a company by id

--- server/resolvers.js
+++ server/resolvers.js
@@ -2,7 +2,8 @@ const db = require('./db');
 
 const Query = {
     jobs: () => db.jobs.list(),
-    job: (root, args) => db.jobs.get(args.id)
+    job: (root, args) => db.jobs.get(args.id),
+    company: (root, args) => db.companies.get(args.id)
 }
 
 // naming is important! In schema, company is inside of the Job. So here it should be same.
