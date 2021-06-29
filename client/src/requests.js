b56ec2613ab60dc0bdc13c9154c96a42d05deb16
Ali Bulut <ali@alibulut.net>
2021-06-29 15:52:04 +0300

    refactoring on client/requests.js and handling graphql error responses

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,51 +1,52 @@
 const endpointUrl = "http://localhost:9000/graphql";
 
-export const loadJobs = async () => {
+const graphqlRequest = async (query, variables={}) => {
     const response = await fetch(endpointUrl, {
         method: "POST",
         headers: {
             "content-type": "application/json"
         },
         body: JSON.stringify({
-            query: `{
-                jobs{
-                    id
-                    title
-                    company{
-                        id
-                        name
-                    }
-                }
-            }`
+            query,
+            variables
         })
     })
 
     const responseBody = await response.json();
-    return responseBody.data.jobs;
+    if(responseBody.errors){
+        const message = responseBody.errors.map(error => error.message).join('\n');
+        throw new Error(message);
+    }
+    return responseBody.data;
+}
+
+export const loadJobs = async () => {
+    const query = `{
+        jobs{
+            id
+            title
+            company{
+                id
+                name
+            }
+        }
+    }`
+    const data = await graphqlRequest(query);
+    return data.jobs;
 }
 
 export const loadJob = async (id) => {
-    const response = await fetch(endpointUrl, {
-        method: "POST",
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify({
-            query: `query JobQuery($id: ID!) {
-                job(id: $id) {
-                    id
-                    title
-                    company {
-                        id
-                        name
-                    }
-                    description
-                }
-            }`,
-            variables: { id }
-        })
-    })
-
-    const responseBody = await response.json();
-    return responseBody.data.job;
+    const query = `query JobQuery($id: ID!) {
+        job(id: $id) {
+            id
+            title
+            company {
+                id
+                name
+            }
+            description
+        }
+    }`
+    const data = await graphqlRequest(query, {id});
+    return data.job;
 }
