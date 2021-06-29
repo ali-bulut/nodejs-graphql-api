0c825806ec0f8a4c6bc1fcb80affe0d5bc72456d
Ali Bulut <ali@alibulut.net>
2021-06-29 15:37:42 +0300

    returning a job by id

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,7 +1,8 @@
 const db = require('./db');
 
 const Query = {
-    jobs: () => db.jobs.list()
+    jobs: () => db.jobs.list(),
+    job: (root, args) => db.jobs.get(args.id)
 }
 
 // naming is important! In schema, company is inside of the Job. So here it should be same.
