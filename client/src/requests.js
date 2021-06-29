48cbb148ba2b266bb5626ef847b1f47e0a23923e
Ali Bulut <ali@alibulut.net>
2021-06-29 15:34:12 +0300

    fetching jobs in the client

---
+++ client/src/requests.js
@@ -0,0 +1,25 @@
+const endpointUrl = "http://localhost:9000/graphql";
+
+export const loadJobs = async () => {
+    const response = await fetch(endpointUrl, {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({
+            query: `{
+                jobs{
+                    id
+                    title
+                    company{
+                        id
+                        name
+                    }
+                }
+            }`
+        })
+    })
+
+    const responseBody = await response.json();
+    return responseBody.data.jobs;
+}
